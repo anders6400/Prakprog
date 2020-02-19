2a78441f4d751e91b50b118bf5ddae5ffbf08744
anders6400 <andersploughansen@hotmail.com>
2020-02-19 15:07:15 +0100

    test

--- vector3d/main.cs
+++ vector3d/main.cs
@@ -2,7 +2,7 @@ using System;
 class main{
 	static int Main(){
 	
-		vector3d v = new vector3d(1,1,1);
+		vector3d v = new vector3d(1,1,2);
 		vector3d u = new vector3d(0,1,0);
 		
 		Console.WriteLine(vector3d.dotproduct(u,v));
