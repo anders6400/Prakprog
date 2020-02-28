43c76da0c27fd0cba244966d085e044bdca27cac
anders6400 <andersploughansen@hotmail.com>
2020-02-28 15:55:26 +0100

    update

--- func/main.cs
+++ func/main.cs
@@ -16,21 +16,26 @@ static Func<double,double> f = (x) => Log(x)/Sqrt(x);
 
 static Func<double,double> g = (x) => Exp(-Pow(x,2));
 
-// Func<double,double,double> h = (x) => Pow(Log(1/x),p);
+static double p = 2;
+static Func<double,double> h = (x) => Pow(Log(1/x),p);
 
 static int Main(){
 
         const double inf=System.Double.PositiveInfinity;
         const double ninf=System.Double.NegativeInfinity;
 	    //Error.Write($"f1(0)={f1(0)}, f2(0)={f2(0)}\n");
+        
         double a=0,b=1,result;
         result=quad.o8av(f,a,b,acc:1e-6,eps:1e-6);
         WriteLine(result);
+        
         double c=ninf, d=inf,result1;
         result1=quad.o8av(g,c,d);
         WriteLine(result1);
-        // double result2;
-        // result2 = quad.o8av(h,a,b);
+        
+        double result2;
+        result2 = quad.o8av(h,a,b);
+        WriteLine(result2);
 
     return 0;
 }
