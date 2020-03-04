031c2bc8f6e570bfa7ae1f9a517abd78cdfe3878
anders6400 <andersploughansen@hotmail.com>
2020-03-04 14:51:00 +0100

    updated directories

--- exercises/func/main.cs
+++ exercises/func/main.cs
@@ -4,14 +4,6 @@ using static System.Math;
 class main{
 
 
-// static Func<double,double> makefun(double y)
-// {// makefun returns a function of the type "double delegate(double x)"
-// 	double a=y;
-// 	Func<double,double> f = delegate(double x){return a;};// unused variable
-// 	Func<double,double> g = (x) => a; // equivalent lambda expression
-// 	return g;
-// }
-
 static Func<double,double> f = (x) => Log(x)/Sqrt(x);
 
 static Func<double,double> g = (x) => Exp(-Pow(x,2));
@@ -23,8 +15,6 @@ static int Main(){
 
         const double inf=System.Double.PositiveInfinity;
         const double ninf=System.Double.NegativeInfinity;
-	    //Error.Write($"f1(0)={f1(0)}, f2(0)={f2(0)}\n");
-        
         double a=0,b=1,result;
         result=quad.o8av(f,a,b,acc:1e-6,eps:1e-6);
         WriteLine(result);
